a3b1352753a6e665af84ec0863395dfbc7c99f07
송창용 <thdckdyd123@naver.com>
2025-09-16 18:03:14 +0900

    [FEAT] 검색 결과 UX

--- Mocar/app/src/main/java/com/autoever/mocar/MainActivity.kt
+++ Mocar/app/src/main/java/com/autoever/mocar/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.autoever.mocar
 
+import CarFilter
+import SearchResultScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.autoever.mocar.ui.theme.MocarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            MocarTheme {
+                MaterialTheme {
+                    SearchResultScreen(
+                        manufacturer = "현대",
+                        model = "아반떼",
+                        filter = CarFilter(),
+                        onCarClick = { car ->
+                            // 차량 클릭 시 동작
+                            println("Clicked: ${car.trim}")
+                        },
+                        onFavoriteClick = { car ->
+                            // 찜 버튼 클릭 시 동작
+                            println("찜 변경: ${car.trim}, 상태=${car.isFavorite}")
+                        }
+                    )
+                }
+            }
         }
     }
 }
